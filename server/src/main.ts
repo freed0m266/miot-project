1055a65a0fed97dea63b05e770d241a7b325f089
MatyasVondra <matyas.vondra@omio.com>
2023-12-10 17:21:55 +0100

    Test fetching message with rest api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -12,4 +13,12 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
+
+    axios.get(`${process.env.REST_API_URL}/v1/messages?group_id=6554f8d6f42cee4c65a5d4c7&device_id=656aeaba2b763a192f382b0e&limit=1`, {
+        headers: {
+            'Authorization': `Bearer ${process.env.API_TOKEN}`
+        }
+    })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
 });
