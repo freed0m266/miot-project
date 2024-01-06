6bb8fcff9d0eb8661581cd438e3641a667b6e868
JayJehlicka <jindrich.jehlicka@mibcon.cz>
2024-01-06 10:22:27 +0100

    rename tableIds

--- server/src/controllers/desks.controllers.ts
+++ server/src/controllers/desks.controllers.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express';
 import { getDesksService } from '../services/desks.service';
 
 type RequestParams = {
-  tableIds?: string[]
+  deskIds?: string[]
   interval: { count: number, unit: 'day' | 'week' | 'month' | 'year' }
 }
 export const getDesks: RequestHandler = (req, res, next) => {
